22f1513172ccba5745c81270a2ec48d4b0c244e5
maxslob <maxik2212@gmail.com>
2015-07-15 11:32:04 +0300

    A lot of modifications, 14.07.2015

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,5 +14,22 @@ $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('c
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
     e.preventDefault();
   });
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
 });
 
