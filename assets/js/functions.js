5cf7652426e777f158b450ccf16453e7ad69feff
maxslob <maxik2212@gmail.com>
2015-07-15 12:59:09 +0300

    Contact panel

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,26 +10,18 @@ $(document).ready(function() {
       }
     });
   });
-$('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+
+});
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
     e.preventDefault();
   });
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
-
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
 });
 
+$(document).ready(function(){
+  $('.sliding-panel-button1,.sliding-panel-fade-screen1,.sliding-panel-close1').on('click touchstart',function (e) {
+    $('.sliding-panel-content1,.sliding-panel-fade-screen1').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
