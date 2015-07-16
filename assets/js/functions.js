f0bd4b116b774296b8cb32c48ff1329b15558659
maxslob <maxik2212@gmail.com>
2015-07-16 14:03:37 +0300

    Fix: Panel, nav. Add: jQuerry anim

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,18 +10,37 @@ $(document).ready(function() {
       }
     });
   });
-
-});
-$(document).ready(function(){
-  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+$('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
     e.preventDefault();
   });
-});
-
-$(document).ready(function(){
-  $('.sliding-panel-button1,.sliding-panel-fade-screen1,.sliding-panel-close1').on('click touchstart',function (e) {
+$('.sliding-panel-button1,.sliding-panel-fade-screen1,.sliding-panel-close1').on('click touchstart',function (e) {
     $('.sliding-panel-content1,.sliding-panel-fade-screen1').toggleClass('is-visible');
     e.preventDefault();
   });
+
+  var element = document.getElementById("js-fadeInElement");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $("#js-fadeInElement").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 300;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+
+
 });
+
