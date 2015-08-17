b397172239c2a0f14268c02777de810baea15dc6
maxslob <maxik2212@gmail.com>
2015-08-18 00:26:48 +0300

    Multiple changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -58,10 +58,12 @@ $('#js-mobile-menu').on('click', function(){
 })();
 
 
-var fade = function(){$(this).closest(this).find('text').fadeToggle('fast');};
+/*var fade = function(){$(this).closest(this).find('text').fadeIn('fast');};
+var fade3 = function(){$('li').closest(this).find('text').fadeOut('fast');};
 
     $('li').on('mouseenter', fade);
-    $('li').on('mouseleave', fade);
+    $('li').on('mouseleave', fade3);*/
+
 /*$( document ).ajaxStart(function() {
   
 });*/
@@ -70,7 +72,6 @@ $('.ajax').on('click', function(){
     $.ajax('ajax.html', {
       success: function (response){
           $('.ajax').html(response).slideDown();
-          //$('.isLoading').hide();
           $('.ajax').css('padding', '0');
       }, 
       error: function(request,errorType,errorMessage){console.log('Error: ' + errorType + ' with message: ' + errorMessage);},
@@ -80,10 +81,10 @@ $('.ajax').on('click', function(){
     });
   });
 
-var par = function(){$('li').find('#p').animate({'margin-left': '50px'});};
+/*var par = function(){$('li').find('#p').animate({'margin-left': '30px'});};
 
     $('.page-header, section').on('touchstart', par);
-    $('.page-header, section').on('mouseenter', par);
+    $('.page-header, section').on('mouseenter', par);*/
 
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
@@ -99,6 +100,114 @@ $(function() {
     }
   });
 });
+
+
+
+
+$(function() {
+  /** -----------------------------------------
+   * Modulo del Slider 
+   -------------------------------------------*/
+   var SliderModule = (function() {
+    var pb = {};
+    pb.el = $('#slider');
+    pb.items = {
+      panels: pb.el.find('.slider-wrapper > li'),
+    }
+
+    // Interval del Slider
+    var SliderInterval,
+      currentSlider = 0,
+      nextSlider = 1,
+      lengthSlider = pb.items.panels.length;
+
+    // Constructor del Slider
+    pb.init = function(settings) {
+      this.settings = settings || {duration: 15000};
+      var items = this.items,
+        lengthPanels = items.panels.length,
+        output = '';
+
+      // Insertamos nuestros botones
+      for(var i = 0; i < lengthPanels; i++) {
+        if(i == 0) {
+          output += '<li class="active"></li>';
+        } else {
+          output += '<li></li>';
+        }
+      }
+
+      $('#control-buttons').html(output);
+
+      // Activamos nuestro Slider
+      activateSlider();
+      // Eventos para los controles
+      $('#control-buttons').on('click', 'li', function(e) {
+        var $this = $(this);
+        if(!(currentSlider === $this.index())) {
+          changePanel($this.index());
+        }
+      });
+
+    }
+
+    // Funcion para activar el Slider
+    var activateSlider = function() {
+      SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
+    }
+
+    // Funcion para la Animacion
+    pb.startSlider = function() {
+      var items = pb.items,
+        controls = $('#control-buttons li');
+      // Comprobamos si es el ultimo panel para reiniciar el conteo
+      if(nextSlider >= lengthSlider) {
+        nextSlider = 0;
+        currentSlider = lengthSlider-1;
+      }
+
+      controls.removeClass('active').eq(nextSlider).addClass('active');
+      items.panels.eq(currentSlider).fadeOut('slow');
+      items.panels.eq(nextSlider).fadeIn('slow');
+
+      // Actualizamos los datos del slider
+      currentSlider = nextSlider;
+      nextSlider += 1;
+    }
+
+    // Funcion para Cambiar de Panel con Los Controles
+    var changePanel = function(id) {
+      clearInterval(SliderInterval);
+      var items = pb.items,
+        controls = $('#control-buttons li');
+      // Comprobamos si el ID esta disponible entre los paneles
+      if(id >= lengthSlider) {
+        id = 0;
+      } else if(id < 0) {
+        id = lengthSlider-1;
+      }
+
+      controls.removeClass('active').eq(id).addClass('active');
+      items.panels.eq(currentSlider).fadeOut('slow');
+      items.panels.eq(id).fadeIn('slow');
+
+      // Volvemos a actualizar los datos del slider
+      currentSlider = id;
+      nextSlider = id+1;
+      // Reactivamos nuestro slider
+      activateSlider();
+    }
+
+    return pb;
+   }());
+
+   SliderModule.init({duration: 6000});
+
+});
+
+
+
+
 
 });
 
